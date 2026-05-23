dc2c3ac843ee84ec32b6c67a4e6680e67e8624ee
kc-node <kc-node1@gmail.com>
2026-05-24 00:21:38 +0200

    feat: add new endpoint: analyse for insights to split responsibility

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,7 @@
 # File tells FastAPI = endpoint expects a file upload 
 from fastapi import FastAPI, UploadFile, File 
 from app.services.pdf_service import extract_text_from_pdf
-from app.services.insights_service import analyse_financial_text
-# Used for copying file contents
+from app.services.insights_service import analyse_financial_text# Used for copying file contents
 import shutil 
 # to handle file paths
 from pathlib import Path 
@@ -22,39 +21,38 @@ def root():
 
 # old one: upload => save
 
-# @app.post("/upload")
-# # to know: fastAPI is built around async 
-# async def upload_file(file: UploadFile = File(...)):
-#     # create a full file path e.g. upload/report.pdf
-#     file_path = UPLOAD_DIR / file.filename 
+@app.post("/upload")
+# to know: fastAPI is built around async 
+async def upload_file(file: UploadFile = File(...)):
+    # create a full file path e.g. upload/report.pdf
+    file_path = UPLOAD_DIR / file.filename 
 
-#     # Save uploaded file 
-#     # pdfs and docx are binary files so wb means write binary
-#     with open(file_path, "wb") as buffer:
-#         # Copies uploaded file data into saved file.
-#         shutil.copyfileobj(file.file, buffer)
-#     # response in json
-#     return {
-#         "filename": file.filename,
-#         "message": "File uploaded successfully"
-#     }
+    # Save uploaded file 
+    # pdfs and docx are binary files so wb means write binary
+    with open(file_path, "wb") as buffer:
+        # Copies uploaded file data into saved file.
+        shutil.copyfileobj(file.file, buffer)
+    # response in json
+    return {
+        "filename": file.filename,
+        "message": "File uploaded successfully"
+    }
 
 # updated one: upload => save => extract text
 
-@app.post("/upload")
-async def upload_file(file: UploadFile = File(...)):
-    file_path = UPLOAD_DIR / file.filename
+@app.get("/analyse/{filename}")
+async def analyse_file(filename: str):
+    file_path = UPLOAD_DIR / filename
 
-    with open(file_path, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
-
+    # Extract text from PDF
     extracted_text = extract_text_from_pdf(file_path)
 
+    # Analyse financial text
     insights = analyse_financial_text(extracted_text)
 
     return {
-        "filename": file.filename,
-        "extracted_text": extracted_text,
+        "filename": filename,
+        # "extracted_text": extracted_text,
         "insights": insights
     }
 
